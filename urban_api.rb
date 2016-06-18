1dce7aa2e467af3a470f59b6dc9d09f00660f7b8
Marco Adkins <madkins@virtualhold.com>
2016-06-17 23:01:44 -0400

    Fixed attachment formatting

--- urban_api.rb
+++ urban_api.rb
@@ -7,13 +7,13 @@ class UrbanApi < Sinatra::Base
   get '/define' do
     content_type :json
     response = JSON.parse(RestClient.get('http://api.urbandictionary.com/v0/define?term=' + params['text']))
-    { response_type: 'in_channel', text: "*Top three definitions for #{params['text']}*", attachments: [mrkdwn_in: ['text'], text: get_definitions(response)] }.to_json
+    { response_type: 'in_channel', text: "*Top three urban definitions for #{params['text']}*", attachments: [mrkdwn_in: ['text', 'fields'], text: get_definitions(response)] }.to_json
   end
 
   def get_definitions response
     definitions = ''
     (0..2).each do |i|
-      definitions += "*#{(i+1).to_s}. *" + response['list'][i]['definition'] + "\n"
+      definitions += "*#{(i+1).to_s}.* " + response['list'][i]['definition'] + "\n"
     end
     definitions
   end
