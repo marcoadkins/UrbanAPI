1a879a2cdb924ab37ab95a29db1362d6b2605096
Marco Adkins <madkins@virtualhold.com>
2016-06-17 22:57:06 -0400

    enabling markdown

--- urban_api.rb
+++ urban_api.rb
@@ -7,7 +7,7 @@ class UrbanApi < Sinatra::Base
   get '/define' do
     content_type :json
     response = JSON.parse(RestClient.get('http://api.urbandictionary.com/v0/define?term=' + params['text']))
-    { response_type: 'in_channel', text: "*Top three definitions for #{params['text']}*", attachments: [text: get_definitions(response)] }.to_json
+    { response_type: 'in_channel', text: "*Top three definitions for #{params['text']}*", attachments: [mrkdwn_in: ['text'], text: get_definitions(response)] }.to_json
   end
 
   def get_definitions response
