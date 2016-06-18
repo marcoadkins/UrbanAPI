0780eedddde544586640eddf8a079d3a33604890
Marco Adkins <madkins@virtualhold.com>
2016-06-17 22:31:57 -0400

    Formatted slack response

--- urban_api.rb
+++ urban_api.rb
@@ -4,36 +4,15 @@ require 'sinatra'
 
 set port: 4444
 
-get '/define/:term' do
-  response = JSON.parse(RestClient.get('http://api.urbandictionary.com/v0/define?term=' + params['term']))
-  { definitions: get_definitions(response, nil) }.to_json
+get '/define' do
+  response = JSON.parse(RestClient.get('http://api.urbandictionary.com/v0/define?term=' + params['text']))
+  { response_type: 'in_channel', text: "Top three definitions for: #{params['text']}", attachments: [text: get_definitions(response)] }.to_json
 end
 
-get '/define/:term/:number' do
-  response = JSON.parse(RestClient.get('http://api.urbandictionary.com/v0/define?term=' + params['term']))
-  { definitions: get_definitions(response, params['number'].to_i) }.to_json
-end
-
-def get_definitions response, number = nil
-  number ||= 0
-  definitions = []
-  (0..number).each do |i|
-    definitions << response['list'][i]['definition']
+def get_definitions response
+  definitions = ''
+  (0..2).each do |i|
+    definitions += response['list'][i]['definition'] + '\n'
   end
   definitions
 end
-
-# {
-#     "response_type": "in_channel",
-#     "text": "Here are the top three definitions:",
-#     "attachments": [
-#         {
-#             "text": "#123456 http://domain.com/ticket/123456 \n
-#             #123457 http://domain.com/ticket/123457 \n
-#             #123458 http://domain.com/ticket/123458 \n
-#             #123459 http://domain.com/ticket/123459 \n
-#             #123460 http://domain.com/ticket/123460"
-#         }
-#     ]
-# }
-
