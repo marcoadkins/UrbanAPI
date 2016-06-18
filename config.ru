39a5413078da91841ac5d9e108031e40fc777138
Marco Adkins <madkins@virtualhold.com>
2016-06-17 22:40:33 -0400

    Config.ru for running app with rack

---
+++ config.ru
@@ -0,0 +1,3 @@
+require File.expand_path('urban_api', File.dirname(__FILE__))
+
+run UrbanApi
--- urban_api.rb
+++ urban_api.rb
@@ -1,16 +1,19 @@
 require 'json'
 require 'rest-client'
-require 'sinatra'
+require 'sinatra/base'
 
-get '/define' do
-  response = JSON.parse(RestClient.get('http://api.urbandictionary.com/v0/define?term=' + params['text']))
-  { response_type: 'in_channel', text: "Top three definitions for: #{params['text']}", attachments: [text: get_definitions(response)] }.to_json
-end
 
-def get_definitions response
-  definitions = ''
-  (0..2).each do |i|
-    definitions += response['list'][i]['definition'] + '\n'
+class UrbanApi < Sinatra::Base
+  get '/define' do
+    response = JSON.parse(RestClient.get('http://api.urbandictionary.com/v0/define?term=' + params['text']))
+    { response_type: 'in_channel', text: "Top three definitions for: #{params['text']}", attachments: [text: get_definitions(response)] }.to_json
   end
-  definitions
+
+  def get_definitions response
+    definitions = ''
+    (0..2).each do |i|
+      definitions += response['list'][i]['definition'] + '\n'
+    end
+    definitions
+  end
 end
