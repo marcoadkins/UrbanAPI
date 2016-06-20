b9f2a1460ae6e896ae6e24cd58051d913d7b7a3d
Marco Adkins <madkins@virtualhold.com>
2016-06-20 13:07:43 -0400

    Added some logic to handle unfound definitions

--- urban_api.rb
+++ urban_api.rb
@@ -13,13 +13,21 @@ class UrbanApi < Sinatra::Base
   get '/define' do
     content_type :json
     response = JSON.parse(RestClient.get('http://api.urbandictionary.com/v0/define?term=' + params['text']))
-    { response_type: 'in_channel', text: "*Top three urban definitions for #{params['text']}*", attachments: [mrkdwn_in: ['text', 'fields'], text: get_definitions(response)] }.to_json
+    { response_type: 'in_channel', text: "*Top urban definitions for #{params['text']}*", attachments: [mrkdwn_in: ['text', 'fields'], text: get_definitions(response)] }.to_json
   end
 
   def get_definitions response
     definitions = ''
-    (0..2).each do |i|
-      definitions += "*#{(i+1).to_s}.* " + response['list'][i]['definition'] + "\n"
+    begin
+      if response['list'].count > 2
+        (0..2).each do |i|
+          definitions += "*#{(i+1).to_s}.* " + response['list'][i]['definition'] + "\n"
+        end
+      else response['list'].count > 0
+        definitions += "*#{(i+1).to_s}.* " + response['list'][0]['definition']
+      end
+    rescue
+      definitions += "*No definitions found.....*"
     end
     definitions
   end
